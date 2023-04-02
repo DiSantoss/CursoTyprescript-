6b95b81b32dd122a7cfb9ec27612518cb3ed9fa5
Di <d.2120leandro@gmail.com>
2023-04-02 00:04:29 -0300

    feat atualizacao-aula3e4

---
+++ 2_Modulo - Typescript Basico/3_Aula/src/script.ts
@@ -0,0 +1,13 @@
+let nomes = ['Diego','Pedro','Luiz','Fulano','Beutrano'];
+
+//para type de array, devemos definir o tipo e colocarmos o [] para dizer que é array.
+
+let outrosNomes: string[] = ['Diego','Pedro','Luiz','Fulano','Beutrano'];
+
+let idades: number[] = [90,15,20,30];
+
+let numeros: Array<number> = [15,23,90,11];
+
+//para manipularmos o array, deve ser com o mesmo tipo.
+
+numeros.push(15);
---
+++ 2_Modulo - Typescript Basico/4_Aula/src/script.ts
@@ -0,0 +1,8 @@
+let nomes1: any = ['Diego','Fran','Jon','Meyer',30];
+
+nomes1.push(40);
+
+//colocamos o any para quando nao queremos especificar um tipo, quando pode ser qualquer tipo.
+
+//nao devemos usar normalmente o any, só em ocasiões especiais.
+
