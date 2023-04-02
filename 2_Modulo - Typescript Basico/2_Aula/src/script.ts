317fd1c04c8afe499b9b0c025d8a3981fbff6dcd
Di <d.2120leandro@gmail.com>
2023-04-01 22:55:09 -0300

    feat atualizacao aula-curso

---
+++ 2_Modulo - Typescript Basico/2_Aula/src/script.ts
@@ -0,0 +1,19 @@
+let nome: string = 'Di';
+
+
+
+
+//Variaveis Primitivas. devemos colocar dois pontos e o tipo da variável depois do nome da variável.
+//ao colocar uma variável definindo o tipo diferente dela, dará error.
+
+// let idade: number = 90;
+
+// let idade = 90;
+
+// if(typeof idade === 'float')
+
+//do tipo boolean.
+
+let programador: boolean = true;
+
+//recomendavel que sempre usemos tudo letra minuscula as tipagens.
